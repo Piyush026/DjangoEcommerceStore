9ae776dadff17611a58beafc072aca77ee0d4288
Piyush026 <piyushjaiswal026@gmail.com>
2020-07-19 10:06:34 +0530

    create templates

---
+++ store/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, HttpResponse
+
+
+# Create your views here.
+
+def store(request):
+    return render(request, 'store/store.html')
+
+
+def cart(request):
+    return render(request, 'store/cart.html')
+
+
+def checkout(request):
+    return render(request, 'store/checkout.html')
