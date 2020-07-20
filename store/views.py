cdbbb7e9983f0ffe66bf03f944d42730cc427ad8
Piyush026 <piyushjaiswal026@gmail.com>
2020-07-20 13:42:05 +0530

    models and templates view

--- store/views.py
+++ store/views.py
@@ -1,15 +1,42 @@
 from django.shortcuts import render, HttpResponse
+from .models import *
 
 
 # Create your views here.
 
 def store(request):
-    return render(request, 'store/store.html')
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'store/store.html', context)
 
 
 def cart(request):
-    return render(request, 'store/cart.html')
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        print(f'customer{customer}')
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        print(f'order{order}')
+        items = order.orderitem_set.all()
+        print(f'items{items}')
+    else:
+        items = []
+        order = {'get_cart_item': 0, 'get_cart_total': 0}
+
+    context = {'items': items, 'order': order}
+    return render(request, 'store/cart.html', context)
 
 
 def checkout(request):
-    return render(request, 'store/checkout.html')
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        print(f'customer{customer}')
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        print(f'order{order}')
+        items = order.orderitem_set.all()
+        print(f'items{items}')
+    else:
+        items = []
+        order = {'get_cart_item': 0, 'get_cart_total': 0}
+
+    context = {'items': items, 'order': order}
+    return render(request, 'store/checkout.html',context)
