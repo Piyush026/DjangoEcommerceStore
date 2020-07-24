585de550b4d4468d9825cf9ab0f1bf5185a3b985
Piyush026 <piyushjaiswal026@gmail.com>
2020-07-24 22:08:55 +0530

    generic cart

--- static/js/cart.js
+++ static/js/cart.js
@@ -10,30 +10,31 @@ for (var i = 0; i < updateBtns.length; i++) {
     if (user ==='AnonymousUser') {
       console.log("user in not authenticated");
     }else {
-      updateUserOrder(productId, action)
+        updateUserOrder(productId, action)
+        // console.log("user in authenticated");
     }
 
   })
 }
 
+
 function updateUserOrder(productId,action) {
-  console.log("user is loged in Sending Data...");
-  var url = '/update_item/'
+    console.log("user is logged in");
+     var url = '/update_item/'
 
-  fetch(url, {
-    method:'POST',
-    headers:{
-      'Content-Type':'application/json',
-      'X-CSRFToken':csrftoken,
-    },
-    body:JSON.stringify({"productId":productId,"action":action})
-  })
-
-  .then((response)=>{
-    return response.json();
-  })
-  .then((data) => {
-    console.log('data',data);
-    location.reload()
-  });
+     fetch(url,{
+       method:"POST",
+       headers:{
+         "Content-Type":"application/json",
+         "X-CSRFToken":csrftoken,
+       },
+       body:JSON.stringify({'productId':productId,'action':action})
+     })
+     .then((response)=>{
+       return response.json()
+     })
+     .then((data)=>{
+       console.log("data:",data);
+//       location.reload()
+     })
 }
